fedda6232037111c055606c42f23dfd9280c436a
r2dev2 <ronak.badhe@gmail.com>
2021-12-14 16:40:23 -0800

    add persistent dialog

--- examples/Dialog.svelte
+++ examples/Dialog.svelte
@@ -1,20 +1,30 @@
 <script>
   import { Button, Dialog } from '..';
 
-  let showDialog = false;
+  let showFirstDialog = false;
+  let showSecondDialog = false;
 </script>
 
-<Dialog bind:value={showDialog}>
+<Dialog bind:value={showFirstDialog}>
   <h5 slot="title">What do you think?</h5>
   <p>I'm not sure about today's weather</p>
   <div slot="actions">
-    <Button text on:click={() => showDialog = false}>Disagree</Button>
-    <Button text on:click={() => showDialog = false}>Agree</Button>
+    <Button text on:click={() => showFirstDialog = false}>Disagree</Button>
+    <Button text on:click={() => showFirstDialog = false}>Agree</Button>
+  </div>
+</Dialog>
+
+<Dialog persistent bind:value={showSecondDialog}>
+  <h5 slot="title">Do you think you can close me by clicking outside?</h5>
+  <p>Dewit</p>
+  <div slot="actions">
+    <Button text on:click={() => showSecondDialog = false}>Hehe boi</Button>
   </div>
 </Dialog>
 
 <div>
-  <Button color="blue" on:click={() => showDialog = true}>Show Dialog</Button>
+  <Button color="blue" on:click={() => showFirstDialog = true}>Show Dialog</Button>
+  <Button color="green" on:click={() => showSecondDialog = true}>Show Persistent Dialog</Button>
 </div>
 
 <style>
