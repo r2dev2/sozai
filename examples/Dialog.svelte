ba12d3cfd6e90dab905673f3d0d773c431c44685
r2dev2 <ronak.badhe@gmail.com>
2021-12-18 20:14:35 -0800

    examplified dialog and textfield examples

--- examples/Dialog.svelte
+++ examples/Dialog.svelte
@@ -1,40 +1,45 @@
 <script>
   import { Button, Dialog } from '..';
+  import Example from './Example.svelte';
+  import Examples from './Examples.svelte';
 
+  const filename = 'Dialog.svelte';
   let showFirstDialog = false;
   let showSecondDialog = false;
 </script>
 
-<Dialog bind:value={showFirstDialog}>
-  <h5 slot="title">What do you think?</h5>
-  <p>I'm not sure about today's weather</p>
-  <div slot="actions">
-    <Button text on:click={() => showFirstDialog = false}>Disagree</Button>
-    <Button text on:click={() => showFirstDialog = false}>Agree</Button>
-  </div>
-</Dialog>
+<Examples>
+  <Example name="basic-dialog" {filename}>
+    <!-- showFirstDialog declared beforehand -->
+    <Dialog bind:value={showFirstDialog}>
+      <h5 slot="title">What do you think?</h5>
+      <p>I'm not sure about today's weather</p>
+      <div slot="actions">
+        <Button text on:click={() => showFirstDialog = false}>Disagree</Button>
+        <Button text on:click={() => showFirstDialog = false}>Agree</Button>
+      </div>
+    </Dialog>
 
-<Dialog persistent bind:value={showSecondDialog}>
-  <h5 slot="title">Do you think you can close me by clicking outside?</h5>
-  <p>Dewit</p>
-  <div slot="actions">
-    <Button text on:click={() => showSecondDialog = false}>Hehe boi</Button>
-  </div>
-</Dialog>
+    <!-- Activator Button -->
+    <Button color="blue" on:click={() => showFirstDialog = true}>Show Dialog</Button>
+  </Example>
+  
+  <Example name="persistent-dialog" {filename}>
+    <Dialog persistent bind:value={showSecondDialog}>
+      <h5 slot="title">Do you think you can close me by clicking outside?</h5>
+      <p>Dewit</p>
+      <div slot="actions">
+        <Button text on:click={() => showSecondDialog = false}>Hehe boi</Button>
+      </div>
+    </Dialog>
 
-<div>
-  <Button color="blue" on:click={() => showFirstDialog = true}>Show Dialog</Button>
-  <Button color="green" on:click={() => showSecondDialog = true}>Show Persistent Dialog</Button>
-</div>
+    <!-- Activator Button -->
+    <Button color="green" on:click={() => showSecondDialog = true}>Show Persistent Dialog</Button>
+  </Example>
+</Examples>
 
 <style>
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-  }
-
-  [slot=actions] {
-    flex-direction: row;
+  h5, p {
+    text-align: left;
   }
 </style>
--- examples/TextField.svelte
+++ examples/TextField.svelte
@@ -1,25 +1,40 @@
 <script>
   import { TextField } from '..';
+  import Example from './Example.svelte';
+  import Examples from './Examples.svelte';
 
-  let value = '';
+  const filename = 'TextField.svelte';
 </script>
 
 <div>
-  <p>Entered: {value}</p>
-  <TextField bind:value label="Basic" />
-  <TextField label="Hint" hint="Test hint" persistentHint color="blue" />
-  <TextField label="Error" error="Test error" />
-  <TextField label="Outlined" outlined />
-  <TextField label="Outlined with hint" outlined hint="Test hint" />
-  <TextField label="Outlined with error" outlined error="Test error" />
-  <TextField label="Outlined textarea" textarea rows="5" outlined />
+  <Examples>
+    <Example name="basic-textfield" {filename}>
+      <TextField label="Basic" />
+    </Example>
+    <Example name="hint-textfield" {filename}>
+      <TextField label="Hint" hint="Test hint" persistentHint color="blue" />
+    </Example>
+    <Example name="error-textfield" {filename}>
+      <TextField label="Error" error="Test error" />
+    </Example>
+    <Example name="outlined-textfield" {filename}>
+      <TextField label="Outlined" outlined />
+    </Example>
+    <Example name="outlined-hint-textfield" {filename}>
+      <TextField label="Outlined with hint" outlined hint="Test hint" />
+    </Example>
+    <Example name="outlined-error-textfield" {filename}>
+      <TextField label="Outlined with error" outlined error="Test error" />
+    </Example>
+    <Example name="outlined-textarea-textfield" {filename}>
+      <!-- Not implemented yet -->
+      <TextField label="Outlined textarea" textarea rows="5" outlined />
+    </Example>
+  </Examples>
 </div>
 
 <style>
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    max-width: min(70%, 48rem);
+  div :global(.example-content) {
+    padding-top: 1rem;
   }
 </style>
