978f5eeec778b6e4221408f7d5dd9463a6003055
r2dev2 <ronak.badhe@gmail.com>
2022-01-30 22:22:06 -0800

    cd: debug replaced index.htmls

--- examples/replacefilepath.sh
+++ examples/replacefilepath.sh
@@ -5,5 +5,8 @@ csssum=$(md5sum build/bundle.css | awk '{print $1}')
 cp build/bundle.js "build/bundle.$jssum.js"
 cp build/bundle.js.map "build/bundle.$jssum.js.map"
 cp build/bundle.css "build/bundle.$csssum.css"
+sed "s/bundle.js/bundle.$jssum.js/g; s/bundle.css/bundle.$csssum.css/g" index.html
 sed -i "s/bundle.js/bundle.$jssum.js/g; s/bundle.css/bundle.$csssum.css/g" index.html
+echo new index
+cat index.html
 sed -i "s/bundle.js.map/bundle.$jssum.js.map/g" "build/bundle.$jssum.js"
