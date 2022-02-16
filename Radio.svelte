fbafa0e50b67db4d2142d3f92e270964009467fc
r2dev2 <r2dev2@users.noreply.github.com>
2022-02-16 17:28:47 +0000

    deploy: 9f680d6e49dcf7b2862d333ec9c4103a0e051e41

--- Radio.svelte
+++ Radio.svelte
@@ -3,15 +3,31 @@
   import Example from './Example.svelte';
   import Examples from './Examples.svelte';
 
+  /** @type {number | null}*/
+  let num = null;
+
   const filename = 'Radio.svelte';
 </script>
 
 <Examples name="Radio">
   <Example name="standard-radio" {filename}>
-    <RadioGroup>
+    <!-- Default Radio -->
+    <RadioGroup
+      bind:selected={num}
+      on:change={(e) => console.log(num, e.detail.selected)}
+    >
       <Radio>Radio 1</Radio>
       <Radio>Radio 2</Radio>
       <Radio>Radio 3</Radio>
+    </RadioGroup>
+  </Example>
+
+  <Example name="colored-radio" {filename}>
+    <!-- Radios can be of different colors -->
+    <RadioGroup>
+      <Radio color="blue">Radio 1</Radio>
+      <Radio color="red">Radio 2</Radio>
+      <Radio color="green">Radio 3</Radio>
     </RadioGroup>
   </Example>
 </Examples>
