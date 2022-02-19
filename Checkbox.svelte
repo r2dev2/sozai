bdb242dc9fe95377ba19f23342eb3c05f79e6919
r2dev2 <r2dev2@users.noreply.github.com>
2022-02-19 01:28:28 +0000

    deploy: 019a82e7f711294a2cf1bb1c9e420a61bba5b94d

---
+++ Checkbox.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Checkbox, CheckboxGroup } from '..';
+  import Example from './Example.svelte';
+  import Examples from './Examples.svelte';
+
+  /** @type {number[]}*/
+  let nums = [];
+
+  const filename = 'Checkbox.svelte';
+</script>
+
+<Examples name="Checkbox">
+  <Example name="standard-checkbox" {filename}>
+    <!-- Default Checkbox -->
+    <CheckboxGroup
+      bind:selected={nums}
+      on:change={(e) => console.log(nums, e.detail.selected)}
+    >
+      <Checkbox>Checkbox 1</Checkbox>
+      <Checkbox>Checkbox 2</Checkbox>
+      <Checkbox>Checkbox 3</Checkbox>
+    </CheckboxGroup>
+  </Example>
+
+  <Example name="colored-checkbox" {filename}>
+    <!-- Checkboxes can be of different colors -->
+    <CheckboxGroup>
+      <Checkbox color="blue">Checkbox 1</Checkbox>
+      <Checkbox color="red">Checkbox 2</Checkbox>
+      <Checkbox color="green">Checkbox 3</Checkbox>
+    </CheckboxGroup>
+  </Example>
+</Examples>
