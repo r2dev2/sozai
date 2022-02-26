817e7185f6939d27e6a59825457208f318649149
r2dev2 <r2dev2@users.noreply.github.com>
2022-02-26 06:09:51 +0000

    deploy: 46ce0cdf8ad11667ac7656c348fd159830e9a6a9

--- Slider.svelte
+++ Slider.svelte
@@ -2,10 +2,14 @@
   import { Slider } from '..';
   import Example from './Example.svelte';
   import Examples from './Examples.svelte';
+
+  let value = 50;
+
+  $: console.log('slider value:', value);
 </script>
 
 <Examples name="Slider" filename="Slider.svelte">
   <Example name="basic-slider">
-    <Slider min={0} max={100} />
+    <Slider min={0} max={100} bind:value />
   </Example>
 </Examples>
