30850d37255b67f65c22b3658df8f4fbdd60a7c2
r2dev2 <ronak.badhe@gmail.com>
2022-03-09 09:19:24 -0800

    feat: customize progress circular with props

--- examples/Progress.svelte
+++ examples/Progress.svelte
@@ -2,13 +2,14 @@
   import { ProgressCircular } from '..';
   import Example from './Example.svelte';
   import Examples from './Examples.svelte';
-
-  let showFirstDialog = false;
-  let showSecondDialog = false;
 </script>
 
 <Examples name="Dialog" filename="Progress.svelte">
   <Example name="progress-indeterminate-round">
     <ProgressCircular indeterminate />
+  </Example>
+
+  <Example name="progress-indeterminate-round-props">
+    <ProgressCircular indeterminate radius={40} thickness={10} color="green" />
   </Example>
 </Examples>
--- src/components/Progress/ProgressCircular.svelte
+++ src/components/Progress/ProgressCircular.svelte
@@ -1,11 +1,16 @@
 <script>
+  import { getColor } from '../../js/utils.js';
+
   export let indeterminate = false;
   export let radius = 20;
-  export let thicc = 5.7;
+  export let thickness = 5.7;
+  export let color = 'var(--primary-color)';
 
-  $: width = (radius + thicc) * 2 + 2;
-  $: center = radius + thicc + 1;
-  $: style = `--circumference: ${2 * Math.PI * radius}px;`;
+  $: width = (radius + thickness) * 2 + 2;
+  $: center = radius + thickness + 1;
+  $: style = `--color: ${getColor(color)}; --circumference: ${
+    2 * Math.PI * radius
+  }px;`;
 </script>
 
 <!-- credit to vuetify for this approach -->
@@ -17,35 +22,43 @@
         cy={center}
         r={radius}
         fill="transparent"
-        stroke="red"
-        stroke-width={thicc}
+        stroke-width={thickness}
       />
     </svg>
   </div>
 {/if}
 
 <style>
+  .s-progressloader-circular {
+    --initial-stroke-dasharray: calc(1.6 * var(--circumference));
+    --stroke-dasharray-1: calc(0.8 * var(--circumference));
+    --stroke-dasharray-2: calc(1.6 * var(--circumference));
+    --halfway-dashoffset: calc(0px - 0.12 - var(--circumference));
+    --full-dashoffset: calc(0px - var(--circumference));
+  }
+
   svg {
     animation: progress-circular-rotate 1.4s linear infinite;
   }
 
   circle {
+    stroke: var(--color);
     stroke-linecap: round;
     animation: progress-circular-dash 1.4s ease-in-out infinite;
   }
 
   @keyframes progress-circular-dash {
     0% {
-      stroke-dasharray: 1, 200;
+      stroke-dasharray: 1, var(--initial-stroke-dasharray);
       stroke-dashoffset: 0px;
     }
     50% {
-      stroke-dasharray: 100, 200;
-      stroke-dashoffset: calc(0px - 0.12 * var(--circumference));
+      stroke-dasharray: var(--stroke-dasharray-1), var(--stroke-dasharray-2);
+      stroke-dashoffset: var(--halfway-dashoffset);
     }
     100% {
-      stroke-dasharray: 100, 200;
-      stroke-dashoffset: calc(0px - var(--circumference));
+      stroke-dasharray: var(--stroke-dasharray-1), var(--stroke-dasharray-2);
+      stroke-dashoffset: var(--full-dashoffset);
     }
   }
 
